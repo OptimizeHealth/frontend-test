2ddd609d686a51e42d757822f35d0da5fda1dc5f
Steven Wong <stevenw00@users.noreply.github.com>
2021-06-22 10:21:10 +0800

    Create airports.js

---
+++ airports.js
@@ -0,0 +1,131 @@
+var airports = [{
+    "name": "Ishigaki (ISG), Japan",
+    "code": "ISG",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}, {
+    "name": "Jeju (CJU), South Korea",
+    "code": "CJU",
+    "countryName": "South Korea",
+    "countryCode": "KR"
+}, {
+    "name": "Chiang Rai (CEI), Thailand",
+    "code": "CEI",
+    "countryName": "Thailand",
+    "countryCode": "TH"
+}, {
+    "name": "Hong Kong (HKG), Hong Kong SAR",
+    "code": "HKG",
+    "countryName": "Hong Kong SAR",
+    "countryCode": "HK"
+}, {
+    "name": "Fukuoka (FUK), Japan",
+    "code": "FUK",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}, {
+    "name": "Osaka (KIX), Japan",
+    "code": "KIX",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}, {
+    "name": "Tokyo (NRT), Japan",
+    "code": "NRT",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}, {
+    "name": "Pusan (PUS), South Korea",
+    "code": "PUS",
+    "countryName": "South Korea",
+    "countryCode": "KR"
+}, {
+    "name": "Seoul (ICN), South Korea",
+    "code": "ICN",
+    "countryName": "South Korea",
+    "countryCode": "KR"
+}, {
+    "name": "Chiang Mai (CNX), Thailand",
+    "code": "CNX",
+    "countryName": "Thailand",
+    "countryCode": "TH"
+}, {
+    "name": "Phuket (HKT), Thailand",
+    "code": "HKT",
+    "countryName": "Thailand",
+    "countryCode": "TH"
+}, {
+    "name": "Da Nang (DAD), Vietnam",
+    "code": "DAD",
+    "countryName": "Vietnam",
+    "countryCode": "VN"
+}, {
+    "name": "Taichung (RMQ), Taiwan",
+    "code": "RMQ",
+    "countryName": "Taiwan",
+    "countryCode": "TW"
+}, {
+    "name": "Tokyo (TYO), Japan",
+    "code": "TYO",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}, {
+    "name": "Tokyo (HND), Japan",
+    "code": "HND",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}, {
+    "name": "Saipan (SPN), United States",
+    "code": "SPN",
+    "countryName": "United States",
+    "countryCode": "US"
+}, {
+    "name": "Ningbo (NGB), Mainland China",
+    "code": "NGB",
+    "countryName": "Mainland China",
+    "countryCode": "CN"
+}, {
+    "name": "Hiroshima (HIJ), Japan",
+    "code": "HIJ",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}, {
+    "name": "Kagoshima (KOJ), Japan",
+    "code": "KOJ",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}, {
+    "name": "Nagoya (NGO), Japan",
+    "code": "NGO",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}, {
+    "name": "Takamatsu (TAK), Japan",
+    "code": "TAK",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}, {
+    "name": "Nha-Trang (CXR), Vietnam",
+    "code": "CXR",
+    "countryName": "Vietnam",
+    "countryCode": "VN"
+}, {
+    "name": "Siem Reap (REP), Cambodia",
+    "code": "REP",
+    "countryName": "Cambodia",
+    "countryCode": "KH"
+}, {
+    "name": "Nagasaki (NGS), Japan",
+    "code": "NGS",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}, {
+    "name": "Bangkok (BKK), Thailand",
+    "code": "BKK",
+    "countryName": "Thailand",
+    "countryCode": "TH"
+}, {
+    "name": "Miyako (Shimojishima) (SHI), Japan",
+    "code": "SHI",
+    "countryName": "Japan",
+    "countryCode": "JP"
+}]
